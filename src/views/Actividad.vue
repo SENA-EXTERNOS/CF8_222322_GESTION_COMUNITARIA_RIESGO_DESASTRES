dab270e33419f387bde1395989138f60f3c1a147
Camilo Bolaño <80546125+elrilacho@users.noreply.github.com>
2023-04-21 08:50:39 -0500

    Componente listo

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -8,7 +8,7 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/curso/temas/img-actividad-didactica.png", alt="Texto que describa la imagen")
+            img(src="@/assets/template/completar-espacios.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo.color-primario
           h2 Gestión y medidas de reducción de riesgos de desastres
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -25,7 +25,7 @@
 
     figure
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/0A5Hr2X6s8Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 
     
